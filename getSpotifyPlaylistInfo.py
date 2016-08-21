655af034b84bc5be290ee48270a47e0d195fdf45
moocarme <moocarme@gmail.com>
2016-08-21 13:55:34 -0400

    update music rec

--- getSpotifyPlaylistInfo.py
+++ getSpotifyPlaylistInfo.py
@@ -14,20 +14,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-# API details 
+# =====================================
+
+# API details and log in ======================
 scope = 'user-library-read'
 token = util.prompt_for_user_token('Matt Moocarme', scope, 
-                                   client_id = 'ca7b704534b94eaa8a5ff45763423971', 
-                                   client_secret = '3d575d58d08440ea94afbbb81972f57f',
+                                   client_id = '-client-id-', 
+                                   client_secret = '-client-secret-',
                                    redirect_uri = 'https://www.google.com')
 
 sp = spotipy.Spotify(auth = token)
 
-# get categories
-categories = sp.categories(limit = 50)
+# ===========================================
 
 # use max limit
 limit = 50
+
+# get categories
+categories = sp.categories(limit = limit)
 
 # Get list of category ids
 ids = []
@@ -52,26 +56,33 @@ for category_id in ids:
 # Initialise lists
 playlist_descs, playlist_names, playlist_followers = [], [], []
 
-# Iterate through playlists
+# Iterate through playlists and get all relevent info
 for playlist_id, playlist_owner in zip(playlist_ids, playlist_owners):
     the_playlist = sp.user_playlist(user = playlist_owner, 
                                     playlist_id = playlist_id)
     playlist_names.append(the_playlist['name'])        # get name
     playlist_descs.append(the_playlist['description']) # get description
     playlist_followers.append(the_playlist['followers']['total']) # get total followers
-    
 
-# Create wordclouds
+# =======================================
+
+# Create wordclouds =====================
 all_names = ' '.join(playlist_names)
-wordcloud = WordCloud().generate(all_names)
+wordcloud = WordCloud(width = 700, height = 300, 
+                      max_font_size=90, relative_scaling=.7).generate(all_names)
 plt.figure(13)
 plt.imshow(wordcloud)
+plt.axis('off')
 
 playlist_descs = ['' if desc is None else desc for desc in playlist_descs]
 all_descs = ' '.join(playlist_descs)
 wordcloud_descs = WordCloud().generate(all_descs)
 plt.figure(-13)
 plt.imshow(wordcloud_descs)
+plt.axis('off')
+# =======================================
+
+# Exploratory daya analysis =============
 
 # find mean number of followers per owner
 df_dict = {'Name' : playlist_names, 
@@ -81,19 +92,23 @@ df_dict = {'Name' : playlist_names,
            
 df = pd.DataFrame(df_dict)
 
-mean_followers_per_user = df.groupby(['Owner']).mean()
+mean_followers_per_user = df.groupby(['Owner']).mean().sort('followers', ascending = False)
 plt.figure(222); plt.clf()
 plt.bar(range(len(mean_followers_per_user)), mean_followers_per_user['followers'])
 
-mean_followers_per_category = df.groupby(['category']).mean()
+mean_followers_per_category = df.groupby(['category']).mean().sort('followers', ascending = False)
 plt.figure(223); plt.clf()
 plt.bar(range(len(mean_followers_per_category)), 
         np.round(mean_followers_per_category['followers']))
 
-# dump in pickle file
+# ========================================
+        
+        
+# dump in pickle file ====================
 pickleFile = open('playlistData.p', 'w')
 pickle.dump((playlist_names, playlist_descs, playlist_followers, 
              playlist_ids, playlist_owners, playlist_metadata, 
              playlist_categories), pickleFile)
 pickleFile.close()
 
+# ========================================
